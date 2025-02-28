d6575678890069da44ef2fbbe97a16a1e355f4ae
Mert Kaan Kuyumcuoglu <mert.kuyumcuoglu@hotmail.com>
2025-02-28 13:11:57 +0100

    Implementation of POST

--- src/main/java/com/example/fobidb/teacher/TeacherRepository.java
+++ src/main/java/com/example/fobidb/teacher/TeacherRepository.java
@@ -1,6 +1,7 @@
 package com.example.fobidb.teacher;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,6 +9,7 @@ import java.util.Optional;
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
 
-    // findTeacherByEmail ist eine SQL Abfrage (SELECT....)
+    // findTeacherByEmail ist eine SQL Abfrage @Query("SELECT s FROM Teacher s WHERE s.email = ?1")
     Optional<Teacher> findTeacherByEmail(String email);
+
 }
--- src/main/java/com/example/fobidb/teacher/TeacherService.java
+++ src/main/java/com/example/fobidb/teacher/TeacherService.java
@@ -2,6 +2,7 @@ package com.example.fobidb.teacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TeacherService {
@@ -18,6 +19,11 @@ public class TeacherService {
     }
 
     public void addNewTeacher(Teacher teacher) {
-        System.out.println(teacher);
+       Optional<Teacher> teacherByEmail = teacherRepository
+           .findTeacherByEmail(teacher.getEmail());
+       if (teacherByEmail.isPresent()) {
+           throw new IllegalStateException("Email exists already");
+       }
+       teacherRepository.save(teacher);
     }
 }
