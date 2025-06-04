30961a677034620265a98830c316c24f9ca63467
pladde <pladde@DESKTOP-LS2R4SQ>
2025-06-04 09:17:19 +0200

    added auth function

--- src/main/java/com/example/fobidb/service/AuthService.java
+++ src/main/java/com/example/fobidb/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.example.fobidb.service;
 
+import com.example.fobidb.dto.LoginRequest;
 import com.example.fobidb.dto.RegisterRequest;
 import com.example.fobidb.entity.Teacher;
 import com.example.fobidb.repository.TeacherRepository;
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
+
 /**
  * Die AuthService-Klasse für die Authentifizierungslogik (Registrierung, Login, Passwort ändern).
  * Hier findet die eigentliche Geschäftslogik statt, z.B. Passwort-Hashing und Datenbank-Interaktion.
@@ -22,7 +25,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 @RequiredArgsConstructor
-@Configuration
 public class AuthService {
 
     private final TeacherRepository teacherRepository;
@@ -50,5 +52,31 @@ public class AuthService {
 
         // Überträgt das Lehrerobjekt an die Datenbank
         return teacherRepository.save(newTeacher);
+    }
+
+    // user login
+    @Transactional
+    // bisher ohne Rückgabewert
+    public void userLogin(LoginRequest loginRequest) {
+
+        String email = loginRequest.getEmail();
+        String rawPassword = loginRequest.getPassword();
+
+        Optional<Teacher> optionalTeacher = teacherRepository.findTeacherByEmail(email);
+
+        if(optionalTeacher.isEmpty()) {
+            throw new RuntimeException("E-Mail oder Passwort ungültig");
+        }
+
+        Teacher teacher = optionalTeacher.get();
+
+        if(passwordEncoder.matches(rawPassword, teacher.getPasswordHash())) {
+            // Login wäre hier erfolgreich -- print ist nur Platzhalter
+            System.out.println("Login erfolgreich");
+        }
+        else {
+            throw new RuntimeException("E-Mail oder Passwort ungültig");
+            // lieber BadCredentialsException verwenden?
+        }
     }
 }
